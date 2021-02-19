7295a4268416254ae372fc57ef72894855ea928c
millnerr <32333237+millnerr@users.noreply.github.com>
2021-02-19 11:11:57 -0500

    Moved the advertising landing page content type out of the theme repository and into the plugin repository.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Elms' Plugins
+# Plugins
 This controls the customized plugins used on elms.edu. Each plugin will have its own directory to keep things organized. When checking these out on the site, use the subdirectory option in WPPusher.
 
-## Folders and What The Plugins Do
-Folder | What It Does
----- | ----
-directory | This controls the [directory on elms.edu](https://www.elms.edu/directory/) and the Personnel content type that controls it.
+## What each plugin does
+Folder | WordPress Name | What It Does
+---- | ---- | ----
+directory | Elms Directory Plugin | This controls the [directory on elms.edu](https://www.elms.edu/directory/) and the Personnel content type that controls it. This only needs to be activated on elms.edu.
+advertising-landing-pages | Advertising Landing Pages | This is a basic custom content type that uses the /lp/ prefix. This only needs to be activated if a page-like content type is needed on the site. 
---
+++ advertising-landing-pages/advertising-landing-page.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+Plugin Name: Advertising Landing Pages
+Description: Controls the advertising landing page content type. This is a full-width (no sidebar), Gutenberg-editor enabled content type whose pages use the /lp/ URL prefix.
+Version: 1.0
+Author: Ryan Millner <millnerr@elms.edu>
+*/
+
+/**
+ * Define the plugin version
+ */
+define("Advertising Landing Pages", "1.0");
+
+  //we only want the advertising landing page  subsite
+  if ( $GLOBALS[ "blog_id" ] == 1 ) {
+    function cptui_register_my_cpts_lp() {
+      /**
+       * Post Type: Advertising Landing Page.
+       */
+      $labels = [
+        "name" => __( "Advertising Landing Page", "gs_elms" ),
+        "singular_name" => __( "Advertising Landing Page", "gs_elms" ),
+      ];
+      $args = [
+        "label" => __( "Advertising Landing Page", "gs_elms" ),
+        "labels" => $labels,
+        "description" => "",
+        "public" => true,
+        "publicly_queryable" => true,
+        "show_ui" => true,
+        "delete_with_user" => false,
+        "show_in_rest" => true,
+        "rest_base" => "",
+        "rest_controller_class" => "WP_REST_Posts_Controller",
+        "has_archive" => false,
+        "show_in_menu" => true,
+        "show_in_nav_menus" => true,
+        "delete_with_user" => false,
+        "exclude_from_search" => true,
+        "capability_type" => "page",
+        "map_meta_cap" => true,
+        "hierarchical" => false,
+        "rewrite" => [ "slug" => "lp", "with_front" => true ],
+        "query_var" => true,
+        "menu_icon" => "dashicons-admin-page",
+        "supports" => [ "title", "editor" ],
+      ];
+      register_post_type( "lp", $args );
+    }
+    add_action( 'init', 'cptui_register_my_cpts_lp' );
+  }
+
+?>
